dcdb87b52a18710ae116fa9ec698d346348b3b4b
Ardenich <36920567+Ardenich@users.noreply.github.com>
2018-05-06 08:10:13 +0700

    Add files via upload

---
+++ ProjectChickenSled/Assets/TerrainHugger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainHugger : MonoBehaviour {
+    public Transform backLeft;
+    public Transform backRight;
+    public Transform frontLeft;
+    public Transform frontRight;
+    public RaycastHit lr;
+    public RaycastHit rr;
+    public RaycastHit lf;
+    public RaycastHit rf;
+
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Physics.Raycast(backLeft.position + Vector3.up, Vector3.down, out lr);
+        Physics.Raycast(backRight.position + Vector3.up, Vector3.down, out rr);
+        Physics.Raycast(frontLeft.position + Vector3.up, Vector3.down, out lf);
+        Physics.Raycast(frontRight.position + Vector3.up, Vector3.down, out rf);
+
+        Debug.DrawRay(rr.point, Vector3.up);
+        Debug.DrawRay(lr.point, Vector3.up);
+        Debug.DrawRay(lf.point, Vector3.up);
+        Debug.DrawRay(rf.point, Vector3.up);
+
+        Vector3 a = rr.point - lr.point;
+        Vector3 b = rf.point - rr.point;
+        Vector3 c = lf.point - rf.point;
+        Vector3 d = rr.point - lf.point;
+
+        Vector3 crossBA = Vector3.Cross(b, a);
+        Vector3 crossCB = Vector3.Cross(c, b);
+        Vector3 crossDC = Vector3.Cross(d, c);
+        Vector3 crossAD = Vector3.Cross(a, d);
+
+        transform.up = (crossBA + crossCB + crossDC + crossAD).normalized;
+
+    }
+}
